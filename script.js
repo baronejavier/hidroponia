45800e8c1e4099bc588fbc236172881e84660312
baronejavier <74462330+baronejavier@users.noreply.github.com>
2026-01-14 13:31:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 const precios = [
-  { id: 1, valor: 139900 },
-  { id: 2, valor: 184900 },
+  { id: 1, valor: 144900 },
+  { id: 2, valor: 189900 },
 ];
 
 const descuentoNormal = 0;
@@ -30,6 +30,7 @@ barraTexto.innerHTML = "🌱 El futuro de tus plantas empieza aquí";
 contadorTexto.textContent = "💧 Tecnología y cuidado en cada planta";
 contadorTiempo.innerHTML = "";
 barraContador.innerHTML = "";
+
 
 
 
