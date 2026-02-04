f33db6decb7ef175cc4a12585e9522a54bfc9570
baronejavier <74462330+baronejavier@users.noreply.github.com>
2026-02-04 13:52:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 const precios = [
-  { id: 1, valor: 139900 },
-  { id: 2, valor: 179900 },
-  { id: 3, valor: 104900 },
+  { id: 1, valor: 144900 },
+  { id: 2, valor: 189900 },
+  { id: 3, valor: 109900 },
 ];
 
 precios.forEach(prod => {
@@ -29,4 +29,5 @@ contadorTexto.textContent = "💧 Tecnología y cuidado en cada planta";
 contadorTiempo.innerHTML = "";
 
 barraContador.innerHTML = "";
+
 
