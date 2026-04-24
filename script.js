70b70d5568a063fc71af2073c5827413a51d400c
baronejavier <74462330+baronejavier@users.noreply.github.com>
2026-04-24 15:13:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 const precios = [
-  { id: 1, valor: 144900 },
-  { id: 2, valor: 184900 },
-  { id: 3, valor: 124900 },
+  { id: 1, valor: 139900 },
+  { id: 2, valor: 174900 },
+  { id: 3, valor: 114900 },
 ];
 
 precios.forEach(prod => {
