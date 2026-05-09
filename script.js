eb567969f294cd0eb6d2e14a7e536d60c4aa26fa
baronejavier <74462330+baronejavier@users.noreply.github.com>
2026-05-09 09:45:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -197,9 +197,9 @@ if (horasRestantes < 12) {
 }
 
 const preciosBase = {
-  p1: 150000,
-  p2: 195000,
-  p3: 135000
+  p1: 140000,
+  p2: 175000,
+  p3: 115000
 };
 
 function formatearPrecio(num) {
